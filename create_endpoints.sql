3bf0e19b40e9a01687e0790e7ad822ee1b987ecb
Celina <45062047+cel-testacc@users.noreply.github.com>
2023-09-19 23:03:51 +0800

    Update create_endpoints.sql

--- create_endpoints.sql
+++ create_endpoints.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS endpoints;
-CREATE TABLE endpoints (
+DROP TABLE IF EXISTS hold_endpoints;
+CREATE TABLE hold_endpoints (
   id INT AUTO_INCREMENT NOT NULL,
   endpoint_path VARCHAR(100) NOT NULL,
   verb VARCHAR(10) NOT NULL,
